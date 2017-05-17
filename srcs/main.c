a14406e1e463a7f7c91b9d1c1bca81288b89cc8a
Tamanh BUI <tbui@e1r9p10.42.fr>
2017-05-17 14:23:55 +0200

    [UPD] still need to figure out the exact output and formatting of recursive in combination with long listing format

--- srcs/main.c
+++ srcs/main.c
@@ -8,7 +8,7 @@ int			main(int ac, char **av)
 	//t_stack		*new;
 
 	i = parse_flags(ac, av, flags);
-	files = alloc_list("."); // TODO handle sorting of files
+	files = alloc_list("."); // TODO add the blocksize for files in a directory (only the ones you show) an show as total
 	output_module(files, flags);
 	return (0);
 }
