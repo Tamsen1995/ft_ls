e6926c4bb467463107c87f23f7b3aa2b6d936b69
Tamanh BUI <tbui@e1r9p10.42.fr>
2017-05-17 10:34:28 +0200

    [UPD] Fixed the problem with the allocation of the list. Turns out it wasn't in the sorting at all but rather in the concatention of the directory path

--- srcs/alloc_list.c
+++ srcs/alloc_list.c
@@ -8,7 +8,7 @@ char			*make_path_dir(char *name, char *cathis)
 	l = ft_strlen(name) + ft_strlen(cathis);
 	l = l + 1;
 	nw_path = NULL;
-	if (!(nw_path = (char *)malloc(sizeof(char) * l)))
+	if (!(nw_path = (char *)malloc(sizeof(char) * l + 1)))
 		exit (-1);
 	nw_path = ft_strcpy(nw_path, name);
 	nw_path = ft_strcat(nw_path, "/");
@@ -27,9 +27,13 @@ t_stack  *register_fls_in_dir(char *name)
 
 	fls = NULL;
 	if (!(dir = opendir(name)))
+	{
 		exit (-1);
+	}
 	if (!(ent = readdir(dir)))
+	{
 		exit (-1);
+	}
 	if (!(fls = ft_lstnew(ent, name)))
 		exit (-1);
 	while ((ent = readdir(dir)))
@@ -52,8 +56,10 @@ t_stack			*alloc_list(char *dir_path)
 	t_stack			*fls;
 	t_stack 		*tmp; // This pointer serves as a tmp pointer for the recursion
 
+
+	//ft_putendl(dir_path);
+
 	tmp = NULL;
-	fls = NULL;
 	if (!(fls = register_fls_in_dir(dir_path))) // zapping the entire entry list into a stack chain
 		exit (-1);
 	tmp = fls;
@@ -61,9 +67,11 @@ t_stack			*alloc_list(char *dir_path)
 	{
 		tmp->path = make_path_dir(dir_path, tmp->filename); // concatenating the path into the path pointer in struct
 		tmp->fields = get_file_info(tmp); // extracting all the info
-		if (not_curr_and_prev(tmp) == TRUE && tmp->type == DT_DIR)
+		if (not_curr_and_prev(tmp) == TRUE && tmp->type == DIRECTORY)
+		{
 			tmp->subdir = alloc_list(tmp->path); // recursively calling the function again with the newly made path in the stack elem
-		tmp = tmp->next; 
+		}
+		tmp = tmp->next;
 	}
 	return (fls); // returnning null for now, just testing
 }
