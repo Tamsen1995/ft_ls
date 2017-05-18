8e76582e2dbc6d4ace48547687c9326e62f66a18
Tamanh BUI <tbui@e1r9p10.42.fr>
2017-05-18 14:04:23 +0200

    [UPD] Managed the output of the total block size and also fixed the bug which would mess up the order of files being printed within the subdirectories.

--- srcs/main.c
+++ srcs/main.c
@@ -7,6 +7,12 @@ int			main(int ac, char **av)
 	t_stack		*files;
 	//t_stack		*new;
 
+	// TODO code function which will check the validity of an input if there is one, meaning it will check if it is even a directory or not
+	// TODO handle the intake of directories as arguments
+
+	// if (ac <)
+
+
 	i = parse_flags(ac, av, flags);
 	files = alloc_list("."); // TODO add the blocksize for files in a directory (only the ones you show) an show as total
 	output_module(files, flags);
