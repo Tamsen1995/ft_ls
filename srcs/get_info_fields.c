d241c9d881ef0b1098307411826eba2b988647d6
Tamanh BUI <tbui@e3r9p10.42.fr>
2017-07-17 17:45:50 +0200

    [UPD] file for handling a single user input normed

--- srcs/get_info_fields.c
+++ srcs/get_info_fields.c
@@ -1,9 +1,6 @@
 #include "../includes/ft_ls.h"
 
-
-
-// a function that simply allocates the size of t_fields which is the structure in the stack elems containing all the necessary info
-t_fields *alloc_fields(void)
+t_fields        *alloc_fields(void)
 {
 	t_fields *fields;
     
@@ -13,12 +10,7 @@ t_fields *alloc_fields(void)
 	return (fields);
 }
 
-
-// this function simply takes the info extracted from stat and returns the owner of the file
-// in the form of a string
-// unfortunately malloc cannot be avoided here since we never know the exact length of the 
-// username
-char                 *extract_owner(struct stat buf)
+char            *extract_owner(struct stat buf)
 {
     struct passwd *pwd;
     uid_t uid;
@@ -28,8 +20,7 @@ char                 *extract_owner(struct stat buf)
     return (pwd->pw_name);
 }
 
-
-char                *extract_group(struct stat buf)
+char            *extract_group(struct stat buf)
 {
     struct     group *grp;
     gid_t      gid;
@@ -39,12 +30,9 @@ char                *extract_group(struct stat buf)
     return(grp->gr_name);
 }
 
-// This function allocates a string which represents the permissions within 
-// stat buffer / st.mode
-// AVOID MALLOC HERE!
-void                extract_permissions_mode(struct stat buf, t_stack *file)
+void            extract_permissions_mode(struct stat buf, t_stack *file)
 {
-    char *string;
+    char        *string;
 
     string = file->fields->mode;
     string[0] = *((S_ISDIR(buf.st_mode)) ? "d" : "-");
@@ -64,15 +52,11 @@ void                extract_permissions_mode(struct stat buf, t_stack *file)
     string[8] = *((buf.st_mode & S_IWOTH) ? "w" : "-");
     string[9] = *((buf.st_mode & S_IXOTH) ? "x" : "-");
     string[10] = '\0';
-
-    // Use the S_ISLINK function later
-   // printf("The file %s a symbolic link\n", (S_ISLNK(buf.st_mode)) ? "is" : "is not");
 }
 
-// gets the size of the file
-char                *extract_file_size(struct stat buf)
+char            *extract_file_size(struct stat buf)
 {
-    char *file_size;
+    char       *file_size;
 
     file_size = NULL;
     if (!(file_size = ft_itoa(buf.st_size)))
@@ -80,10 +64,9 @@ char                *extract_file_size(struct stat buf)
     return (file_size);
 }
 
-// gets the number of hard links into a string
-char                *extract_nbr_of_links(struct stat buf)
+char             *extract_nbr_of_links(struct stat buf)
 {
-    char *nbr_links;
+    char        *nbr_links;
 
     nbr_links = NULL;
     if (!(nbr_links = ft_itoa(buf.st_nlink)))
@@ -91,20 +74,15 @@ char                *extract_nbr_of_links(struct stat buf)
     return (nbr_links);
 }
 
-// This function acts as an extractor of information
-// It extracts all the necessary information from a file by redirecting to subfunctions which will be respoinsible
-// for extracting the specific information
 t_fields			*get_file_info(t_stack *file)
 {
+    t_stack         *tmp;
+    struct stat     buf;           
 
-	// a temp pointer for sanitary and isolated modification of the file
-	t_stack *tmp;
-	struct stat buf;           
-
-	tmp = file;
-	tmp->fields = alloc_fields(); // simply a function that allocates the fields in the stack element		
-	if (lstat(tmp->path, &buf) < 0)
-		exit(-1);
+    tmp = file;
+    tmp->fields = alloc_fields(); 
+    if (lstat(tmp->path, &buf) < 0)
+        exit(-1);
     extract_permissions_mode(buf, tmp);
     extract_date_time(buf, tmp->fields);
     tmp->fields->st_blocks = (long long int)buf.st_blocks;
