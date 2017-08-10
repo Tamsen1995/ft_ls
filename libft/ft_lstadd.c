a297cb1d30e01440a9befd234860a759aa6eb35b
Tamanh BUI <tbui@e1r8p1.42.fr>
2017-08-10 19:28:43 +0200

    [UPD] Norming the project

--- libft/ft_lstadd.c
+++ libft/ft_lstadd.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/10 19:05:18 by tbui              #+#    #+#             */
+/*   Updated: 2017/08/10 19:07:05 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../includes/libft.h"
 
@@ -6,20 +17,22 @@
 ** Without sorting it
 ** is only used if the f flag has been activated
 */
-void		ft_lstadd(t_stack **begin_list, struct dirent *ent, char *path, char *flags)
+
+void		ft_lstadd(t_stack **begin_list, \
+struct dirent *ent, char *path, char *flags)
 {
-    t_stack *tmp;
+	t_stack *tmp;
 	t_stack *new;
 
-    tmp = NULL;
+	tmp = NULL;
 	new = ft_lstnew(ent, path, flags);
 	if (!*begin_list)
 	{
 		*begin_list = ft_lstnew(ent, path, flags);
 		return ;
 	}
-    tmp = *begin_list;
-    while (tmp->next)
-        tmp = tmp->next;
-    tmp->next = new;
+	tmp = *begin_list;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
 }
